de17b3417cea2b782b20441e4cfea402de163d35
mattRCB <mattsteinrcb@gmail.com>
2017-05-01 18:28:50 -0400

    revised test

--- test/App.spec.js
+++ test/App.spec.js
@@ -7,13 +7,9 @@ import App from '../src/components/App';
 describe('Component: App', () => {
   it('should render the App component', () => {
     const wrapper = shallow(
-      <App name="World" />
+      <App />
     );
 
-    expect(wrapper.find('h1').text()).toEqual('Hello, World!');
   });
 
-  it('should run a solid smoke test', () => {
-    expect(true).toEqual(false);
-  });
 });
