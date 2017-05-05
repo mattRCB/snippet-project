c9137eedd0c385f822619ad21fc1cb0536f32f6d
mattRCB <mattsteinrcb@gmail.com>
2017-05-05 16:15:47 -0400

    testing fetch

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -29,7 +29,7 @@ class App extends Component {
 				'Accept': 'application/json',
 				'Content-Type': 'application/json'
 			},
-			body: {"title": "Snippet Test"}
+			body: JSON.stringify({title: "Snippet Test", desc: "This is a description"})
 		});
 	}
 
